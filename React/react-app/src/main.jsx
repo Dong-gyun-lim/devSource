84b1f92ad05301c4b6766f311134b6a182433472
Dong-gyun-lim <sfeagle130@naver.com>
2025-08-11 16:53:22 +0900

    8월11일 실습

--- React/react-app/src/main.jsx
+++ React/react-app/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 // import App from './App.jsx';
 // import MyComp from './App00.jsx';
-import App from './App10.jsx';
+import App from './App14.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
