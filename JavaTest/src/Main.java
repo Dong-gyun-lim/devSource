815fcb516a4b25ac225d3ff7fc67212ad7a46b3e
Dong-gyun-lim <sfeagle130@naver.com>
2025-08-27 16:44:06 +0900

    8월27일 실습

---
+++ JavaTest/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("안녕 자바~");
+
+        String str = "반가워요~";
+        String str2 = new String("Hello");
+
+    }
+}
